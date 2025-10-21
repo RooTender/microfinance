329f04774c0c341b57196981c15aef1b89bfd015
RooTender <therootender@gmail.com>
2025-10-21 11:40:04 +0200

    Move to basic frame

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Overview from "./Overview.svelte";
+    import Transactions from "./Transactions.svelte";
+
+</script>
+<main>
+	<Overview/>
+	<Transactions/>
+	<Transactions/>
+</main>
